2a6334b510b41d4f149b1cce9e47d6b511554ad0
HaseebK733 <haseeb.khan3@ontariotechu.net>
2025-12-29 12:27:25 -0500

    Added new vercel URL to the Server

--- server.py
+++ server.py
@@ -52,7 +52,7 @@ def on_startup():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://roast-my-code-taupe.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
